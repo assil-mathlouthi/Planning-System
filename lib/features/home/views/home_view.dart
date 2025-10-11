6d4e7733369844271adfccb05c4d9e21ea780ea1
assil <astradhdbjd@gmail.com>
2025-10-11 21:04:46 +0100

    add the home view

---
+++ lib/features/home/views/home_view.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:planning_system/features/home/widgets/home_view_body.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: HomeViewBody(),
+    );
+  }
+}
