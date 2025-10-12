0451434ef25e97d029e23f094f0cf39b95abd48e
Houssein Msallem <houssein.msallem@gmail.com>
2025-10-12 15:38:03 +0100

    removed toMap and fromMap methods, changed attributes to camel case

--- lib/core/models/Enseignant.dart
+++ lib/core/models/Enseignant.dart
@@ -1,71 +1,55 @@
 import 'package:planning_system/core/models/enums.dart';
 
 class Enseignant {
-  String code_smartex_ens;
-  String nom_ens;
-  String prenom_ens;
-  String email_ens;
-  Grade grade_code_ens;
-  bool participe_surveillance;
+  String codeSmartexEns;
+  String nomEns;
+  String prenomEns;
+  String emailEns;
+  Grade gradeCodeEns;
+  bool participeSurveillance;
   Enseignant({
-    required this.code_smartex_ens,
-    required this.nom_ens,
-    required this.prenom_ens,
-    required this.email_ens,
-    required this.grade_code_ens,
-    required this.participe_surveillance,
+    required this.codeSmartexEns,
+    required this.nomEns,
+    required this.prenomEns,
+    required this.emailEns,
+    required this.gradeCodeEns,
+    required this.participeSurveillance,
   });
-  Map<String, dynamic> toMap() {
-    return {
-      "code_smartex_ens": code_smartex_ens,
-      "nom_ens": nom_ens,
-      "prenom_ens": prenom_ens,
-      "email_ens": email_ens,
-      "grade_code_ens": grade_code_ens,
-      "participe_surveillance": participe_surveillance,
-    };
-  }
-
-  factory Enseignant.fromMap(Map<String, dynamic> map) {
-    return Enseignant(
-      code_smartex_ens: map['code_smartex_ens'],
-      nom_ens: map['nom_ens'],
-      prenom_ens: map['prenom_ens'],
-      email_ens: map['email_ens'],
-      grade_code_ens: map['grade_code_ens'],
-      participe_surveillance: map['participe_surveillance'],
-    );
-  }
+  
   Enseignant copyWith({
-    String? code_smartex_ens,
-    String? nom_ens,
-    String? prenom_ens,
-    String? email_ens,
-    Grade? grade_code_ens,
-    bool? participe_surveillance,
+    String? codeSmartexEns,
+    String? nomEns,
+    String? prenomEns,
+    String? emailEns,
+    Grade? gradeCodeEns,
+    bool? participeSurveillance,
   }) {
     return Enseignant(
-      code_smartex_ens: code_smartex_ens ?? this.code_smartex_ens,
-      nom_ens: nom_ens ?? this.nom_ens,
-      prenom_ens: prenom_ens ?? this.prenom_ens,
-      email_ens: email_ens ?? this.email_ens,
-      grade_code_ens: grade_code_ens ?? this.grade_code_ens,
-      participe_surveillance:
-          participe_surveillance ?? this.participe_surveillance,
+      codeSmartexEns: codeSmartexEns ?? this.codeSmartexEns,
+      nomEns: nomEns ?? this.nomEns,
+      prenomEns: prenomEns ?? this.prenomEns,
+      emailEns: emailEns ?? this.emailEns,
+      gradeCodeEns: gradeCodeEns ?? this.gradeCodeEns,
+      participeSurveillance:
+          participeSurveillance ?? this.participeSurveillance,
     );
   }
 
   @override
   String toString() {
-    return 'Enseignant (code_smartex_ens:$code_smartex_ens,nom_ens:$nom_ens ,prenom_ens:$prenom_ens,email_ens:$email_ens,grade_code_ens:$grade_code_ens,participe_surveillance:$participe_surveillance )';
+    return 'Enseignant (codeSmartexEns:$codeSmartexEns,nomEns:$nomEns ,prenomEns:$prenomEns,emailEns:$emailEns,gradeCodeEns:$gradeCodeEns,participeSurveillance:$participeSurveillance )';
   }
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
     return other is Enseignant &&
-        other.code_smartex_ens == code_smartex_ens &&
-        other.email_ens == email_ens;
+        other.codeSmartexEns == codeSmartexEns &&
+        other.emailEns == emailEns;
   }
-  
+
+  @override
+  int get hashCode {
+    return codeSmartexEns.hashCode ^ emailEns.hashCode;
+  }
 }
