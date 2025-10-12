8ed7a78c13a33e79f0ddbc34940222e9182baa0c
Houssein Msallem <houssein.msallem@gmail.com>
2025-10-12 15:52:52 +0100

    EnseignantMatiere model

---
+++ lib/core/models/EnseignantMatiere.dart
@@ -0,0 +1,30 @@
+class EnseignantMatiere {
+  String codeMatiere;
+  String codeSmartexEns;
+  EnseignantMatiere({required this.codeMatiere, required this.codeSmartexEns});
+  EnseignantMatiere copyWith({String? codeMatiere, String? codeSmartexEns}) {
+    return EnseignantMatiere(
+      codeMatiere: codeMatiere ?? this.codeMatiere,
+      codeSmartexEns: codeSmartexEns ?? this.codeSmartexEns,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Enseignant (codeSmartexEns: $codeSmartexEns, est affecté a la matiere $codeMatiere).';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is EnseignantMatiere &&
+        other.codeMatiere == codeMatiere &&
+        other.codeSmartexEns == codeSmartexEns;
+  }
+
+  @override
+  // TODO: implement hashCode
+  int get hashCode {
+    return codeMatiere.hashCode ^ codeSmartexEns.hashCode;
+  }
+}
