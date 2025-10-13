dbce92e6ca5e3b5b619c8c39bb267270e08ba052
Houssein Msallem <houssein.msallem@gmail.com>
2025-10-13 12:09:14 +0100

    added conditional enum widget logic in cell content, needs to be modified

--- lib/core/common/cell_content.dart
+++ lib/core/common/cell_content.dart
@@ -1,18 +1,51 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:planning_system/core/enums/grade.dart';
+import 'package:planning_system/core/enums/seance.dart';
+import 'package:planning_system/core/enums/semestre.dart';
+import 'package:planning_system/core/enums/session.dart';
 import 'package:planning_system/core/extensions/color_scheme_shorthand.dart';
 import 'package:planning_system/core/utils/app_style.dart';
 
 class CellContent extends StatelessWidget {
   final dynamic content;
   const CellContent({super.key, required this.content});
-  // Widget ReturnByEnum(Enum enumValue){
-  // Ya3ni yraja3 widget styles wla yraja3 style hasb el enum eli yet3adelo
-  //yaani ken session yraj3 widget wla style specifiee lil session
-  // wo baed el build method titcheki ithaken el cell content enum, ylanci el method hethi
-  //bch yaarf el widget wla el style eli yelzem yhotou fil cell
-  // }
+  Widget ReturnByEnum(Enum enumValue, BuildContext context) {
+    BoxDecoration variable = BoxDecoration(color: Colors.white);
+    if (enumValue is Grade) {
+      variable = BoxDecoration(
+        color: context.colors.onPrimaryFixed,
+        border: Border.all(color: context.colors.onPrimary),
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+      );
+    } else if (enumValue is Seance) {
+      variable = BoxDecoration(
+        color: context.colors.onPrimaryFixed,
+        border: Border.all(color: context.colors.onPrimary),
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+      );
+    } else if (enumValue is Session) {
+      variable = BoxDecoration(
+        color: context.colors.onPrimaryFixed,
+        border: Border.all(color: context.colors.onPrimary),
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+      );
+    } else if (enumValue is Semestre) {
+      variable = BoxDecoration(
+        color: context.colors.onPrimaryFixed,
+        border: Border.all(color: context.colors.onPrimary),
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+      );
+    }
+    return Container(decoration: variable, child: Text(content.toString()));
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (content is Enum) {
+      print(content);
+      return ReturnByEnum(content, context);
+    }
     if (content is Widget) {
       return content;
     } else {
