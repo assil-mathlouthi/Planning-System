565883e20c458dfbc80fb017275baca4cb16888a
aconitum001 <skandern0000@gmail.com>
2025-10-17 23:43:58 +0100

    added edit button to grade statics card

--- lib/features/enseignant/widgets/grade_statics_card.dart
+++ lib/features/enseignant/widgets/grade_statics_card.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:planning_system/core/extensions/color_scheme_shorthand.dart';
 import 'package:planning_system/core/extensions/gap_with_sized_box.dart';
 import 'package:planning_system/core/utils/app_style.dart';
 import 'package:planning_system/features/enseignant/models/grade_stat_model.dart';
+import 'package:planning_system/features/enseignant/widgets/dialog_button.dart';
+import 'package:planning_system/features/enseignant/widgets/grade_dialog.dart';
 import 'package:planning_system/features/enseignant/widgets/total_grade_card.dart';
 
 class GradeStaticsCard extends StatelessWidget {
@@ -20,6 +23,7 @@ class GradeStaticsCard extends StatelessWidget {
         border: Border.all(color: context.colors.tertiary),
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -31,7 +35,7 @@ class GradeStaticsCard extends StatelessWidget {
               TotalGradeCard(total: model.total, grade: model.gradeEnum),
             ],
           ),
-          42.h,
+          22.h,
           8.h,
           _buildRow(
             context,
@@ -45,6 +49,18 @@ class GradeStaticsCard extends StatelessWidget {
             "nb de Séance:",
             model.nbOfSeance.toString(),
             Color(0xff2C5AA0),
+          ),
+          18.h,
+          SizedBox(
+            height: 32,
+            child: DialogButton(
+              text: "Editer",
+              bgColor: context.colors.primary,
+              textColor: context.colors.onPrimary,
+              onPressed: () {
+                Get.dialog(GradeDialog(gradeStatModel: model));
+              },
+            ),
           ),
         ],
       ),
--- lib/features/enseignant/widgets/grade_statistics.dart
+++ lib/features/enseignant/widgets/grade_statistics.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:planning_system/core/extensions/gap_with_sized_box.dart';
 import 'package:planning_system/features/enseignant/controllers/enseignant_controller.dart';
-import 'package:planning_system/features/enseignant/widgets/grade_dialog.dart';
 import 'package:planning_system/features/enseignant/widgets/grade_statics_card.dart';
 
 class GradeStatistics extends GetView<EnseignantController> {
@@ -11,18 +10,13 @@ class GradeStatistics extends GetView<EnseignantController> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 170,
+      height: 200,
       child: ListView.separated(
         scrollDirection: Axis.horizontal,
         itemCount: controller.grades.length,
         shrinkWrap: true,
         itemBuilder: (context, index) {
-          return InkWell(
-            onTap: () {
-              Get.dialog(GradeDialog(gradeStatModel: controller.grades[index]));
-            },
-            child: GradeStaticsCard(model: controller.grades[index]),
-          );
+          return GradeStaticsCard(model: controller.grades[index]);
         },
         separatorBuilder: (context, index) => 16.w,
       ),
