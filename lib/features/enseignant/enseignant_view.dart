62226841c6001a1b69134db16738d64aaf06c082
assil <astradhdbjd@gmail.com>
2025-10-18 11:56:45 +0100

    Refactor EnseignantView to use EnseignantMainContent and remove unnecessary imports

--- lib/features/enseignant/enseignant_view.dart
+++ lib/features/enseignant/enseignant_view.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'package:planning_system/core/extensions/color_scheme_shorthand.dart';
 import 'package:planning_system/features/enseignant/controllers/enseignant_controller.dart';
 import 'package:planning_system/features/enseignant/widgets/eneigant_header.dart';
-import 'package:planning_system/features/enseignant/widgets/enseignant_table.dart';
+import 'package:planning_system/features/enseignant/widgets/enseignant_main_content.dart';
 import 'package:planning_system/features/enseignant/widgets/grade_statistics.dart';
 import 'package:planning_system/core/controller/table_controller.dart';
 
@@ -12,7 +12,6 @@ class EnseignantView extends GetView<EnseignantController> {
 
   @override
   Widget build(BuildContext context) {
-    
     Get.put(TableController(), tag: "Ens");
     return Container(
       color: context.colors.surface,
@@ -24,11 +23,10 @@ class EnseignantView extends GetView<EnseignantController> {
           children: [
             EneigantHeader(),
             GradeStatistics(),
-            EnseignantTable(),
+            EnseignantMainContent(),
           ],
         ),
       ),
     );
   }
 }
-
