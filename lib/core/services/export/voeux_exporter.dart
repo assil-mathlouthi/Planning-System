63508465953708e5925ee40987371fdbf64a1c30
assil <astradhdbjd@gmail.com>
2025-10-18 16:31:33 +0100

    some fixes

--- lib/core/services/export/voeux_exporter.dart
+++ lib/core/services/export/voeux_exporter.dart
@@ -1,18 +1,18 @@
 import 'package:drift/drift.dart' show QueryRow;
+import 'package:planning_system/core/enums/semestre.dart';
 import 'package:planning_system/core/interface/exporter_interface.dart';
 
 class VoeuxExporter implements ExcelExporter<QueryRow> {
   const VoeuxExporter();
 
   @override
-  List<String> get headers => const [	
+  List<String> get headers => const [
     'Nom',
     'Prénom',
     'Session',
     'Semestre',
     'jour',
     'Séance',
-   
   ];
 
   @override
@@ -20,10 +20,9 @@ class VoeuxExporter implements ExcelExporter<QueryRow> {
     item.read<String>('nomEns'),
     item.read<String>('prenomEns'),
     item.read<String>('session'),
-    item.read<String>('semestre'),
+    SemestreEnum.parseSemestre(item.read<String>('semestre')).getText,
     item.read<int>('jour'),
-    item.read<String>('seance'),
-   
+    item.read<String>('seance').toUpperCase(),
   ];
 
   @override
